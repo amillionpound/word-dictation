e673d25a7aba73484f54c879d5e606767787d874
amillionpound <snowsky@qq.com>
2026-07-13 13:12:34 +0800

    fix: disable debug mode for SCF runtime

--- app.py
+++ app.py
@@ -1851,4 +1851,4 @@ def main_handler(event, context):
     return app(event, context)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000, debug=True)
+    app.run(host='0.0.0.0', port=9000, debug=False)
